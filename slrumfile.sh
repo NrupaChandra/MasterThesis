66eb9b9f2c9012571c25c36a0c1fe4f240c2d996
NrupaChandra <nrupa.chandra@stud.tu-darmstadt.de>
2024-12-02 14:27:11 +0100

    slrumfile
    s

---
+++ slrumfile.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH -J Test              # Job name
+
+#
+#SBATCH -e /work/home/ng66sume/MasterThesis/%x.err.%j.txt
+#SBATCH -o /work/home/ng66sume/MasterThesis/%x.out.%j.txt
+#
+# CPU Request:
+#SBATCH -n 1                                # 1 MPI process/task
+#SBATCH -c 4                                # Number of CPU cores (OpenMP threads) per MPI process
+#SBATCH --mem-per-cpu=3800                  # Memory in MB per core
+#SBATCH -t 02:00:00                         # Job runtime limit (hh:mm:ss)
+
+# GPU Request:
+#SBATCH --gres=gpu:v100:1                   # Request 1 GPU of type NVidia V100
+# -------------------------------
+# Your commands to start the computation:
+module purge
+module load gcc cuda tensorflow/2.10.0-gpu  # Load necessary modules
+cd /work/home/ng66sume/MasterThesis/
+
+# Set environment variables for thread and GPU management:
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# Check assigned GPUs (output appears in the error log):
+nvidia-smi 1>&2
+
+# Activate your Python environment:
+source ~/miniconda3/bin/activate env     # Replace `my_env` with your Conda environment name
+
+# Run your Python script:
+python Helloworld.py                      # Replace `your_script.py` with your script name
+EXITCODE=$?
+
+# Job script exits with the program's exit code:
+exit $EXITCODE
